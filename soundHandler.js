738dd2eb0d0581e45319980b2cfb101af004bcb5
Alexey Belkin <48679054+avbelkin@users.noreply.github.com>
2022-11-20 14:09:43 +0300

    move to TS, add example

--- soundHandler.js
+++ soundHandler.js
@@ -1,26 +1,63 @@
-﻿L.Handler.PlaySoundHandler = L.Handler.extend({
-    _self: this,
-
-    initialize: function(map, options) {
-        this._map = map;
-        this._options = options;
-        _self = this;
-        if (!map.options.soundsPath)
-            throw new Error('playsound plugin requires map.options.soundsPath parameter to be set');
-        this._map.on('playsound', this._playsound, this);
-    },
-    addHooks: function() {
-    },
-
-    _playsound(e) {
-        var audio = new Audio(`${window.location.origin}${map.options.soundsPath}${e.audioName}.mp3`);
-        audio.crossOrigin = 'anonymous';
-        audio.volume = e.volume ? e.volume : 0.3;
-        L.Util.throttle(audio.play().catch(err => console.warn(err),1000));
+var L;
+(function (L) {
+    let PathKind;
+    (function (PathKind) {
+        PathKind["Absolute"] = "absolute";
+        PathKind["RelativeToSite"] = "site-relative";
+        PathKind["FilesystemRelative"] = "filesystem-relative";
+    })(PathKind = L.PathKind || (L.PathKind = {}));
+    class PlaySoundOptions {
+        constructor() {
+            this.crossOrigin = 'anonymous';
+            this.soundsPath = "/sounds/";
+            this.soundsPathKind = PathKind.RelativeToSite;
+            this.defaultExtension = "mp3";
+            this.defaultVolume = 0.3;
+        }
     }
-});
-
-L.playSoundHandler = function (opts) {
-    return new L.Handler.PlaySoundHandler(opts);
-}
-L.Map.addInitHook('addHandler', 'playSoundHandler', L.playSoundHandler);
+    L.PlaySoundOptions = PlaySoundOptions;
+    class PlaySoundHandler extends L.Handler {
+        initialize(map) {
+            map.options.playSound = Object.assign(Object.assign({}, new PlaySoundOptions()), map.options.playSound);
+            this._map = map;
+            this._self = this;
+        }
+        addHooks() {
+            this._map.on('playsound', this._playsound, this);
+        }
+        removeHooks() {
+            this._map.off('playsound', this._playsound, this);
+        }
+        _playsound(e) {
+            if (this._playing)
+                return;
+            const options = this._map.options.playSound;
+            let audioAddress = null;
+            if (options.soundsPathKind == PathKind.FilesystemRelative) {
+                audioAddress = `${window.location.href}${options.soundsPath}${e.audioName}.${options.defaultExtension}`;
+            }
+            else if (options.soundsPathKind == PathKind.RelativeToSite) {
+                audioAddress = `${window.location.origin}${options.soundsPath}${e.audioName}${options.defaultExtension}`;
+            }
+            else {
+                audioAddress = e.audioName;
+            }
+            var audio = new Audio(audioAddress);
+            audio.crossOrigin = options.crossOrigin;
+            audio.volume = e.volume ? e.volume : options.defaultVolume;
+            this._play.call(this, audio);
+        }
+        _play(audio) {
+            this._playing = audio;
+            audio.play()
+                .catch(err => console.warn(err))
+                .then(() => delete this._playing);
+        }
+    }
+    L.PlaySoundHandler = PlaySoundHandler;
+    function playSoundHandler(opts) {
+        return new L.PlaySoundHandler(opts);
+    }
+    L.playSoundHandler = playSoundHandler;
+    L.Map.addInitHook('addHandler', 'playSoundHandler', L.playSoundHandler);
+})(L || (L = {}));
---
+++ soundHandler.ts
@@ -0,0 +1,94 @@
+﻿namespace L { 
+    export interface Evented {
+        on(type: 'playsound', fn: ((event: PlaysoundEvent) => void), context: PlaySoundHandler) : this;
+        off(type: 'playsound', fn: ((event: PlaysoundEvent) => void), context: PlaySoundHandler): this;
+        /**
+         * plays one sound at once
+         * */
+        fire(type: 'playsound', data: PlaysoundEvent, propagate?: boolean): this;
+    }
+
+    export enum PathKind{
+        Absolute = "absolute",
+        RelativeToSite = "site-relative",
+        FilesystemRelative = "filesystem-relative"
+    }
+
+    export class PlaySoundOptions {
+        /** 
+        * set to null when working on local file system
+        */
+        crossOrigin: string = 'anonymous'
+        soundsPath?: string = "/sounds/";
+        soundsPathKind: PathKind = PathKind.RelativeToSite;
+        /**
+         * for relative paths
+         * */
+        defaultExtension: string = "mp3";
+        /**
+         * Volume: 0-1
+         * */
+        defaultVolume: number = 0.3;
+    }
+
+    export interface MapOptions {
+        
+        playSoundHandler: L.PlaySoundHandler,
+        playSound: PlaySoundOptions
+    }
+    export interface PlaysoundEvent {
+        audioName: string;
+        volume: number;
+    }
+    export class PlaySoundHandler extends L.Handler {
+        _self: PlaySoundHandler;
+        _map: Map;
+        _playing?: HTMLAudioElement;
+
+
+        initialize(map: Map) {
+            map.options.playSound = { ...new PlaySoundOptions(), ...map.options.playSound };
+            this._map = map;
+            this._self = this;
+        }
+
+        addHooks() {
+            this._map.on('playsound', this._playsound, this);
+        }
+        removeHooks() {
+            this._map.off('playsound', this._playsound, this);
+        }
+
+        private _playsound(e: PlaysoundEvent) {
+            if (this._playing) return;
+            
+            const options = this._map.options.playSound;
+            let audioAddress: string = null;            
+            if (options.soundsPathKind == PathKind.FilesystemRelative) {
+                audioAddress = `${window.location.href}${options.soundsPath}${e.audioName}.${options.defaultExtension}`;
+            }
+            else if (options.soundsPathKind == PathKind.RelativeToSite) {
+                audioAddress = `${window.location.origin}${options.soundsPath}${e.audioName}${options.defaultExtension}`;
+            }
+            else {
+                audioAddress = e.audioName;
+            }
+            
+            var audio = new Audio(audioAddress);
+            audio.crossOrigin = options.crossOrigin;
+            audio.volume = e.volume ? e.volume : options.defaultVolume;
+            this._play.call(this, audio);
+        }
+        private _play(this: PlaySoundHandler, audio: HTMLAudioElement) {
+            this._playing = audio;
+            audio.play()
+                .catch(err => console.warn(err))
+                .then(() => delete this._playing);
+        }
+    }
+
+    export function playSoundHandler (opts) {
+        return new L.PlaySoundHandler(opts);
+    }
+    L.Map.addInitHook('addHandler', 'playSoundHandler', L.playSoundHandler);
+}
